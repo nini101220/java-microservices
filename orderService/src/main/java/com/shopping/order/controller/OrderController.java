6ed02a172f8d5a453e071bb69d91358e00462725
nini101220 <nidshah24@gmail.com>
2024-12-06 10:19:29 +0000

    Update OrderController.java

--- orderService/src/main/java/com/shopping/order/controller/OrderController.java
+++ orderService/src/main/java/com/shopping/order/controller/OrderController.java
@@ -1,9 +1,16 @@
 package com.shopping.order.controller;
 
 import com.shopping.model.Order;
+import com.shopping.order.exception.OrderNotFoundExcpetion;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.concurrent.atomic.AtomicLong;
@@ -22,10 +29,26 @@ public class OrderController {
         return order;
     }
 
+    @Operation(summary = "Get a Order by its id")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found the Order",
+                    content = { @Content(mediaType = "application/json",
+                            schema = @Schema(implementation = Order.class)) }),
+            @ApiResponse(responseCode = "404", description = "Order not found",
+                    content = @Content) })
     @GetMapping("/getByOrderId")
-    public Order getByOrderId(@RequestParam String orderId) {
+    public ResponseEntity<Order> getByOrderId(@RequestParam Long orderId) throws OrderNotFoundExcpetion {
+        if(orderId>30){
+           throw new OrderNotFoundExcpetion("Order ID not found");
+        }
+        else{
         Order r = new Order();
+       
+        r.setCustomerId(1L);
+        r.setPrice(10000);
+        r.setStatus("Order delivered");
         LOG.info("Sent: {}", r);
-        return r;
+            return new ResponseEntity<>(r, HttpStatus.OK);
+        }
     }
 }
